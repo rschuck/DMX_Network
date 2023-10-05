d7ecb686a5f291bf763f4f77ddf433079ac2d808
schuck <rogerjschuck@gmail.com>
2023-10-05 12:21:03 -0700

    Working on DMX Node Interfaces

---
+++ DMX_Network/DMX_Light.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMX_Network
+{
+    public class DMX_Light : DXM_Interface
+    {
+        public DMX_Light(string name, byte address) 
+        {
+            Name = name;
+            Address = address;
+            Red = 0;
+            Green = 0;
+            Blue = 0;
+            White = 0;
+            Amber = 0;
+            UV = 0;
+        }
+
+        public override bool InsertData(List<byte> data)
+        {
+            //data.Insert(Address, Red);
+            //data.Insert(Address+1, Green);
+            //data.Insert(Address+2, Blue);
+            //data.Insert(Address+3, White);
+            //data.Insert(Address+4, Amber);
+            //data.Insert(Address+5, UV);
+
+            data[Address] = Red;
+            data[Address + 1] = Green;
+            data[Address + 2] = Blue;
+            data[Address + 3] = White;
+            data[Address + 4] = Amber;
+            data[Address + 5] = UV;
+            return true; 
+        }
+
+        public string Name { get; set; }
+
+        public byte Address { get; set; }
+
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+        public byte White { get; set; }
+        public byte Amber { get; set; }
+        public byte UV { get; set; }
+
+    }
+}
--- DMX_Network/MainWindow.xaml.cs
+++ DMX_Network/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Haukcode.ArtNet.IO;
+using Haukcode.ArtNet.Packets;
 
 namespace DMX_Network
 {
@@ -23,6 +28,41 @@ namespace DMX_Network
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //const int DmxMaxSize = 512;
+            //List<byte> dataArray = new List<byte>(DmxMaxSize);
+            //for (int x=0; x< DmxMaxSize; x++) { dataArray.Insert(x,0); }
+
+            //DMX_Light light1 = new DMX_Light("First Light", 0);
+            //light1.Green = 225;
+            //light1.InsertData(dataArray);
+            //byte[] bytes = dataArray.ToArray();
+
+
+            /////////////////////////////////////////////////////////////////////////////
+
+            //ArtNetDmxPacket dmxPacket = new ArtNetDmxPacket();
+            //dmxPacket.DmxData = bytes;
+
+            //var stream = new MemoryStream();
+            //var artBinaryWriter = new ArtNetBinaryWriter(stream);
+
+            //Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //IPAddress serverAddr = IPAddress.Parse("192.168.1.144");
+            //IPEndPoint endPoint = new IPEndPoint(serverAddr, 6454);
+
+            //stream.SetLength(0);
+            //stream.Flush();
+
+            //dmxPacket.Sequence = 0;
+
+            //dmxPacket.WriteData(artBinaryWriter);
+            //sock.SendTo(stream.GetBuffer(), endPoint);
+
+
         }
     }
 }
